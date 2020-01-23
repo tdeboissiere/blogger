dc86fb1859a4448cc8ba244b1896e1946d6bab39
tdeboissiere <thibault@lyrebird.ai>
2020-01-23 18:06:56 +0100

    Add tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
+
   root to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
